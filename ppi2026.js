876b84409c2473bd1287918a296a2a2e714442d2
Guilherme Santos <106110470+guileals@users.noreply.github.com>
2026-05-06 19:19:19 +0000

    Aula 06/05 - Introdução a Javascript

---
+++ ppi2026.js
@@ -0,0 +1,39 @@
+/*
+    Programação para Internet - 2026
+    Professor: Guilherme Leal
+*/
+
+// Ativar o formatador Prettier
+// ALT + SHIFT + F
+
+// # Aula 06/05 - Introdução a JavaScript
+
+console.log("Bem vindo a PPI!");
+
+// Variáveis
+let name = "João";
+let age = 17;
+let isStudent = true;
+
+const PI = 3.14; // Não pode mudar de valor
+console.log(PI);
+
+// Operadores Aritméticos + - / * ** %
+let x = 5 + 5; // soma
+let y = "5" + 5; // concatenação
+let z = "Hello" + 5;
+
+console.log(x**2, y, z); // ** potência
+console.log(typeof x);
+
+// Operadores relacionais
+console.log('5' != 5); // Diferença
+console.log('5' == 5); // Compara tipo OU valor
+console.log('5' === 5); // Compara tipo E valor
+
+// Operadores de incremento
+console.log(x++); // incremento após o retorno do valor
+console.log(x);
+console.log(++x); // incremento antes do retorno do valor
+
+
