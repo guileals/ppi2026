3ff904ed62f06f3e7f74f7c834f9ae13d8806a85
Guilherme Santos <106110470+guileals@users.noreply.github.com>
2026-05-20 18:44:10 +0000

    Aula 20/05 - Objects, DESAFIO 1

--- ppi2026.js
+++ ppi2026.js
@@ -222,3 +222,41 @@ const pessoa = {
 
 console.log(pessoa.nome);
 console.log(pessoa['nome']);
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+
+const pessoa2 = { ...pessoa, hobby: 'Vaquejada'};
+console.log(pessoa2);
+
+// Atribuição por desestruturação
+const { nome, idade } = pessoa2;
+console.log(nome, idade);
+
+// Construtor - Protótipo de Objeto
+function Product(name, sto, ram, price, qty, category){
+  this.name = name;
+  this.sto = sto;
+  this.ram = ram;
+  this.price = price;
+  this.qty = qty;
+  this.category = category;
+}
+
+const p1 = new Product('iPhone', 256, 16, 5000, 10, 'Celular');
+const p2 = new Product('iPad', 512, 16, 8000, 20, 'Tablet');
+const p3 = new Product('Macbook', 1024, 24, 15000, 30, 'Notebook');
+const p4 = new Product('iPhone Pro Max', 256, 16, 7000, 15, 'Celular');
+const p5 = new Product('iPad Pro', 512, 16, 10000, 25, 'Tablet');
+const p6 = new Product('Macbook Pro', 1024, 24, 25000, 35, 'Notebook');
+
+const estoque = [ p1, p2, p3, p4, p5, p6];
+console.log(estoque);
+
+/* DESAFIO 1
+
+  a) Valor total de todos os produtos em estoque
+  b) Ordene os produtos por nome (crescente/descrescente)
+  c) Ordene os produtos por preço (crescente/descrescente)
+  d) Filtre produtos de acordo uma categoria
+
+*/
